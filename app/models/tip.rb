a7a1000e8f60f4e48c57e2a31361a7fd6fe7cedb
Troy Tschirhart <troytschirhart@Troys-MacBook-Pro.local>
2019-09-06 08:22:50 -0600

    updated db name

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -3,7 +3,7 @@ class Tip
         uri = URI.parse(ENV['DATABASE_URL'])
         DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
     else
-        DB = PG.connect(host: "localhost", port: 5432, dbname: 'sports_book_development')
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'project_four_development')
     end
 
 
