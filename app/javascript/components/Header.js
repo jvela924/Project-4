7fec2cbb13950fe3f993a5a0ecf668746ac71433
Jeremy Vela <jvela924@gmail.com>
2019-09-06 15:04:17 -0400

    adding styling

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -4,10 +4,18 @@ class Header extends React.Component {
   render () {
     return (
       <div className="header">
-        <h1 className="title">
+        <div>
+          <h1 className="title">
           BetShare
-        </h1>
-        <img className="title-image" src="http://sportsthenandnow.com/wp/wp-content/uploads/2018/06/sports-betting-2018.jpg" alt="sports betting" />
+          </h1>
+          <div className="header-flex">
+            <button className="header-button">Home</button>
+            <button className="header-button">Share Your Bet</button>
+          </div>
+        </div>
+        <div>
+          <img className="title-image" src="http://sportsthenandnow.com/wp/wp-content/uploads/2018/06/sports-betting-2018.jpg" alt="sports betting" />
+        </div>
       </div>
     )
   }
