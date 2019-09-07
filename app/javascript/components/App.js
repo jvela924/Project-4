3740337dc54b44e186719db4eb9e825634c27b0a
Troy Tschirhart <troytschirhart@Troys-MacBook-Pro.local>
2019-09-07 10:00:52 -0600

    added a constructor and handleView to App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,6 +5,84 @@ import Main from './Main.js'
 import Score from './Score.js'
 
 class App extends React.Component {
+    // ======================================  State
+    constructor(props) {
+        super(props)
+        this.state = {
+            view: {
+                page: 'home',
+                pageTitle: 'Bets Overview'
+            },
+            formInputs: {
+                id: null,
+                person: null,
+                sidebet: null,
+                opponent: null,
+                hometeam: null,
+                spread: null,
+                odds: null,
+                wager: null,
+                rationale: null
+            }
+        }
+    }
+
+    // ======================================  Handlers
+    // Set the pageTitle and formInputs based on the view
+    handleView = (view, tipData) => {
+        // Set variables to empty values
+        let pageTitle = ''
+        let formInputs = {
+            id: null,
+            person: '',
+            sidebet: '',
+            opponent: '',
+            hometeam: '',
+            spread: null,
+            odds: null,
+            wager: null,
+            rationale: ''
+        }
+        // Switch case to set the pageTitle based on the view
+        switch (view) {
+            case 'home':
+                pageTitle = 'Bets Overview'
+                break
+            case 'seeMore':
+                pageTitle = 'Bet Detail'
+                break
+            case 'addTip':
+                pageTitle = 'Add a Bet'
+                break
+            case 'editTip':
+                pageTitle = 'Edit a Bet'
+                formInputs = {
+                    id: tipData.id,
+                    person: tipData.person,
+                    sidebet: tipData.sideBet,
+                    opponent: tipData.opponent,
+                    hometeam: tipData.hometeam,
+                    spread: tipData.spread,
+                    odds: tipData.odds,
+                    wager: tipData.wager,
+                    rationale: tipData.rationale
+                }
+                break
+            default:
+                break
+        }
+        // update the state
+        this.setState({
+            view: {
+                page: view,
+                pageTitle: pageTitle
+            },
+            formInputs: formInputs
+        })
+    }
+
+
+    // ======================================  Render
   render () {
     return (
       <div>
@@ -20,4 +98,5 @@ class App extends React.Component {
   }
 }
 
+// ==========================================  Export
 export default App
